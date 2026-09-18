5786e29733d34b75cda95169a57a7aad9122a617
Aiman Asghar <aasghar.bese25seecs>
2026-09-18 19:54:17 +0500

    testing the issorted function using arithmatic pointers

--- sorted.cpp
+++ sorted.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cassert>
 bool isSorted(const int* arr, const int size) {
-    return false; // Stub implementation
+    for (int i = 0; i < size - 1; i++) {
+        if (*(arr + i) > *(arr + i + 1)) return false;
+    }
+    return true;
 }
 void testSortedArray() {
     int arr[] = {1, 2, 3, 4, 5};
